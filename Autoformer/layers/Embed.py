94d483bcfc50e88cc42045a1500eea0e9f2b981f
xyh <3190100997@zju.edu.cn>
2022-09-23 17:42:08 +0200

    relative positional embedding

--- Autoformer/layers/Embed.py
+++ Autoformer/layers/Embed.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils import weight_norm
+from einops import rearrange
 import math
 
 
@@ -23,6 +24,96 @@ class PositionalEmbedding(nn.Module):
 
     def forward(self, x):
         return self.pe[:, :x.size(1)]
+
+def relative_to_absolute(q):
+    """
+    Converts the dimension that is specified from the axis
+    from relative distances (with length 2*tokens-1) to absolute distance (length tokens)
+      Input: [bs, heads, length, 2*length - 1]
+      Output: [bs, heads, length, length]
+    """
+    b, h, l, _, device, dtype = *q.shape, q.device, q.dtype
+    dd = {'device': device, 'dtype': dtype}
+    col_pad = torch.zeros((b, h, l, 1), **dd)
+    x = torch.cat((q, col_pad), dim=3)  # zero pad 2l-1 to 2l
+    flat_x = rearrange(x, 'b h l c -> b h (l c)')
+    flat_pad = torch.zeros((b, h, l - 1), **dd)
+    flat_x_padded = torch.cat((flat_x, flat_pad), dim=2)
+    final_x = flat_x_padded.reshape(b, h, l + 1, 2 * l - 1)
+    final_x = final_x[:, :, :l, (l - 1):]
+    return final_x
+
+
+def rel_pos_emb_1d(q, rel_emb, shared_heads):
+    """
+    Same functionality as RelPosEmb1D
+    Args:
+        q: a 4d tensor of shape [batch, heads, tokens, dim]
+        rel_emb: a 2D or 3D tensor
+        of shape [ 2*tokens-1 , dim] or [ heads, 2*tokens-1 , dim]
+    """
+    if shared_heads:
+        emb = torch.einsum('b h t d, r d -> b h t r', q, rel_emb)
+    else:
+        emb = torch.einsum('b h t d, h r d -> b h t r', q, rel_emb)
+    return relative_to_absolute(emb)
+
+
+class RelPosEmb1D(nn.Module):
+    def __init__(self, tokens, dim_head, heads=None):
+        """
+        Output: [batch head tokens tokens]
+        Args:
+            tokens: the number of the tokens of the seq
+            dim_head: the size of the last dimension of q
+            heads: if None representation is shared across heads.
+            else the number of heads must be provided
+        """
+        super().__init__()
+        scale = dim_head ** -0.5
+        self.shared_heads = heads if heads is not None else True
+        if self.shared_heads:
+            self.rel_pos_emb = nn.Parameter(torch.randn(2 * tokens - 1, dim_head) * scale)
+        else:
+            self.rel_pos_emb = nn.Parameter(torch.randn(heads, 2 * tokens - 1, dim_head) * scale)
+
+    def forward(self, q):
+        return rel_pos_emb_1d(q, self.rel_pos_emb, self.shared_heads)
+
+class CausalConv1d(torch.nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=0,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+        
+        self.__padding = (kernel_size - 1) * dilation
+        
+    def forward(self, input):
+        return super(CausalConv1d, self).forward(F.pad(input, (self.__padding, 0)))
+
+
+class context_embedding(torch.nn.Module):
+    def __init__(self,in_channels=1,embedding_size=256,k=5):
+        super(context_embedding,self).__init__()
+        self.causal_convolution = CausalConv1d(in_channels,embedding_size,kernel_size=k)
+
+    def forward(self,x):
+        x = self.causal_convolution(x)
+        return F.tanh(x)
 
 
 class TokenEmbedding(nn.Module):
@@ -107,6 +198,21 @@ class DataEmbedding(nn.Module):
         super(DataEmbedding, self).__init__()
 
         self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
+        self.position_embedding = PositionalEmbedding(d_model=d_model)
+        self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type,
+                                                    freq=freq) if embed_type != 'timeF' else TimeFeatureEmbedding(
+            d_model=d_model, embed_type=embed_type, freq=freq)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x, x_mark):
+        x = self.value_embedding(x) + self.temporal_embedding(x_mark) + self.position_embedding(x)
+        return self.dropout(x)
+
+class ConvEmbedding(nn.Module):
+    def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1):
+        super(DataEmbedding, self).__init__()
+
+        self.value_embedding = context_embedding(in_channels=c_in, embedding_size=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
         self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type,
                                                     freq=freq) if embed_type != 'timeF' else TimeFeatureEmbedding(
